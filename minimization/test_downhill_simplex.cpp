165cafd4ae16b2f34d01531ec74b9d4765b25257
Philipp Lang <philipp.s.lang@gmail.com>
2017-08-25 11:21:14 +0100

    Update test_downhill_simplex.cpp

--- minimization/test_downhill_simplex.cpp
+++ minimization/test_downhill_simplex.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "downhill_simplex.h"
 
-TEST(downhill_simplex, test_one)
+TEST(minimization, downhill_simplex)
 {
 	auto const res = test_me(50.);
 	ASSERT_NEAR(res, 50.*50., 1.0E-10);
