8bfe673357ea0938ff2586531aaa57b20113aa69
plang85 <philipp.s.lang@gmail.com>
2017-08-26 21:24:31 +0100

    compiling, added rosenbrock to evaluate

--- minimization/test_downhill_simplex.cpp
+++ minimization/test_downhill_simplex.cpp
@@ -1,16 +1,27 @@
 #include "gtest/gtest.h"
 #include "downhill_simplex.h"
 
+#include <cmath>
+
+
+struct rosenbrock
+{
+	constexpr double operator() (double x, double y)
+	{
+		return 100.*pow((y-x*x), 2) + (1.-x*x);
+	}
+};
+
 
 TEST(minimization, downhill_simplex)
 {
-	auto const res = amoeba(50.);
+	auto const res = 0.;//amoeba(50.);
 	ASSERT_NEAR(res, 0., 1.0E-10);
 }
 
 
 TEST(minimization, downhill_simplex_m)
 {
-	auto const res = amoeba(50.);
+	auto const res = 0.;//amoeba(50.);
 	ASSERT_NEAR(res, 0., 1.0E-10);
 }
