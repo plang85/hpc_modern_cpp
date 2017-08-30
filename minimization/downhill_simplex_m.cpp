16f2a0de90a0fb58371f041d5f32a8d6ed0cbdec
plang85 <philipp.s.lang@gmail.com>
2017-08-30 21:15:13 +0100

    of course

---
+++ minimization/downhill_simplex_m.cpp
@@ -0,0 +1,15 @@
+#include "downhill_simplex_m.h"
+
+#include <iostream>
+
+
+double amoeba_m(double)
+{
+	return 0.;
+}
+
+
+double ammotry_m(double)
+{
+	return 0.;
+}
---
+++ minimization/downhill_simplex_m.h
@@ -0,0 +1,7 @@
+#ifndef MINIMIZATION_DOWNHILL_SIMPLEX_M
+#define MINIMIZATION_DOWNHILL_SIMPLEX_M
+
+double amoeba_m(double);
+
+#endif
+
