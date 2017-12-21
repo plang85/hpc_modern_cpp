177b7b67f33b2aa729612c89576f6c9531122686
Philipp Lang <plang@slb.com>
2017-12-21 11:39:32 +0000

    blah

--- simd/SIMD.cpp
+++ simd/SIMD.cpp
@@ -1,7 +1,8 @@
 #include "SIMD.h"
 
 #include "cpuid.h"
-
+#include <pmmintrin.h>
+#include <immintrin.h>
 
 cpu_info_t cpu_info(int i)
 {
@@ -14,8 +15,14 @@ cpu_info_t cpu_info(int i)
 }
 
 
-
 void mult(float const *a, float const *b, float *c)
 {
+}
 
+
+void add()
+{
+    auto ra = _mm_set_ps(1, 2, 3, 4);
+    auto rb = _mm_set_ps(4, 3, 2, 1);
+    auto rc = _mm_add_ps(ra, rb);
 }
